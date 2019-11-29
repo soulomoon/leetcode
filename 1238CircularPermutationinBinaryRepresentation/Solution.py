e18947168f43fc90c3219f9a7d26fe3e2c01ace6
soulomoon <fwy996602672@gmail.com>
2019-11-29 21:55:10 +0800

    1238CircularPermutationinBinaryRepresentation fast

--- 1238CircularPermutationinBinaryRepresentation/Solution.py
+++ 1238CircularPermutationinBinaryRepresentation/Solution.py
@@ -3,11 +3,25 @@ from typing import List
 
 class Solution:
     def circularPermutation(self, n: int, start: int) -> List[int]:
-        lst = get_standard(n)
-        print(lst)
-        r = lst
-        index = r.index(start)
-        return r[index:] + r[:index]
+        return [start ^ grey_code(i) for i in range(2 ** n)]
+
+    def circularPermutation_slow(self, n: int, start: int) -> List[int]:
+
+        # lst = get_standard(n)
+        lst = [0]
+        a = 0
+        b = 1
+        for i in range(n):
+            new_lst = []
+            for e in lst:
+                a, b = b, a
+                new_lst.append(a + e)
+                new_lst.append(b + e)
+            lst = new_lst
+            a = a * 2
+            b = b * 2
+        index = lst.index(start)
+        return lst[index:] + lst[:index]
 
 
 def get_standard(n: int) -> [int]:
@@ -25,5 +39,19 @@ def get_standard(n: int) -> [int]:
         return new
 
 
+def grey_code(n):
+    return n ^ (n >> 1)
+
+
 if __name__ == "__main__":
+    print(1 >> 1)
+    print(2 >> 1)
+    print((7 >> 1))
+    print(7 ^ (7 >> 1))
+    # print(g(1))
+    # print(g(2))
+    # print(g(3))
+    # print(g(4))
+    # print(g(5))
+
     print(Solution().circularPermutation(3, 2))
