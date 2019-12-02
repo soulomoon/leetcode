a0912ed3dd5e74b6b5546c2e7b9a2dabcab18c35
soulomoon <fwy996602672@gmail.com>
2019-12-03 04:28:05 +0800

    This is fast push

--- 1240TilingaRectanglewiththeFewestSquares/Solution.py
+++ 1240TilingaRectanglewiththeFewestSquares/Solution.py
@@ -1,4 +1,4 @@
-from math import sqrt
+from math import sqrt, inf
 from time import time
 from typing import List
 
@@ -6,31 +6,54 @@ from typing import List
 # a cheated method full of special case
 class Solution:
     def tilingRectangle(self, n: int, m: int) -> List[int]:
+        return self.solve(n, m)
+
+    def solve(self, n: int, m: int) -> List[int]:
         if n < m:
-            return self.tilingRectangle(m, n)
-        if m == 0:
-            return 0
-        elif n == 13 and m == 11:
-            return 6
-        elif n == 7 and m == 6:
-            return 5
-        elif n == 10 and m == 9:
-            return 6
-        elif n == 11 and m == 10:
-            return 6
-        elif n == 11 and m == 6:
-            return 6
-        elif n == 12 and m == 11:
-            return 7
-        elif n == 13 and m == 12:
-            return 7
-        return 1 + self.tilingRectangle(m, n - m)
+            return self.solve(m, n)
+        self.rez = inf
+
+        ssu = square_sum_up(m * n)
+
+        def dp(skyline: List[int], count, left):
+            if ssu[left] + count >= self.rez:
+                return
+            ground = min(skyline)
+            if ground >= n:
+                # print(n, skyline)
+                self.rez = min(count, self.rez)
+                return
+            begin = skyline.index(ground)
+            size_limit = 1
+            while ground + size_limit < n \
+                    and begin + size_limit < m \
+                    and skyline[begin + size_limit] == ground:
+                size_limit += 1
+            # print(skyline, ground, begin, size_limit, count)
+            for size in range(size_limit, 0, -1):
+                new_skyline = list(skyline)
+                for index in range(begin, begin + size):
+                    new_skyline[index] += size
+                dp(new_skyline, count + 1, left - size ** 2)
+
+        dp([0] * m, 0, m * n)
+        return self.rez
+
+
+def square_sum_up(limit):
+    dp = [-1] * (limit + 1)
+    dp[0] = 0
+    for i in range(1, limit + 1):
+        dp[i] = 1 + min(dp[i - j ** 2] for j in range(1, 1 + int(sqrt(i))))
+    return dp
 
 
 if __name__ == "__main__":
     t = time()
+    print(Solution().solve(2, 3))
     # print(Solution().tilingRectangle(2, 3))
-    print(Solution().tilingRectangle(11, 13))
+    print(Solution().solve(11, 13))
+    # print(Solution().tilingRectangle(11, 13))
     # print(squres_sum_up(143))
     # pprint(Solution().tilingRectangle(5, 8))
     te = time()
