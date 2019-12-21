f6008d2842813135116a33ed8ea022e6a7423463
soulomoon <fwy996602672@gmail.com>
2019-12-21 21:20:11 +0800

    finish 1221

--- 1284MinimumNumberofFlipstoConvertBinaryMatrixtoZeroMatrix/Solution.py
+++ 1284MinimumNumberofFlipstoConvertBinaryMatrixtoZeroMatrix/Solution.py
@@ -10,26 +10,40 @@ class Solution:
         width = len(mat[0])
         n = height * width
         mini = inf
+        mat_number = 0
+        for row in mat:
+            for x in row:
+                mat_number <<= 1
+                mat_number += x
 
-        def op(x, y):
+        def op(x, y, mn):
             for a, b in direction:
                 nx = a + x
                 ny = b + y
                 if width > nx >= 0 and height > ny >= 0:
-                    matcp[ny][nx] ^= 1
+                    mn ^= 1 << (n - (ny * width + nx + 1))
+            return mn
+
+        def genmap():
+            for y in range(height):
+                for x in range(width):
+                    yield op(x, y, 0)
+
+        lst = list(genmap())
 
         for code in range(2 ** n):
-            matcp = [row.copy() for row in mat]
+            mn = mat_number
             count = 0
             for i in range(n):
                 if 0X1 << i & code:
-                    op(i % width, i // width)
+                    mn ^= lst[i]
                     count += 1
-            if all(all(i == 0 for i in row) for row in matcp):
+            if mn == 0:
                 mini = min(mini, count)
         return mini if mini != inf else -1
 
 
 if __name__ == "__main__":
-    mat = [[0, 0], [0, 1]]
+    # mat = [[0, 0], [0, 1]]
+    mat = [[1, 0, 0], [1, 0, 0]]
     print(Solution().minFlips(mat))
