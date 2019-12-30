f5af7211f3963a446c42f6836e3716b19bbfaba5
soulomoon <fwy996602672@gmail.com>
2019-12-30 20:38:44 +0800

    This is fast push

---
+++ 1081SmallestSubsequenceofDistinctCharacters/Solution.py
@@ -0,0 +1,32 @@
+class Solution:
+    def smallestSubsequence(self, text: str) -> str:
+        end_mark = dict()
+        for idx, x in enumerate(text): end_mark[x] = idx
+        ends = sorted(end_mark.values())
+        visited = {}
+        start = -1
+        for end in ends:
+            if text[end] in visited: continue
+            start_mark = {}
+            for i in range(start, end + 1):
+                if text[i] not in visited and text[i] <= text[end]:
+                    start_mark.setdefault(text[i], [])
+                    start_mark[text[i]].append(i)
+            for key in sorted(start_mark.keys()):
+                for i in start_mark[key]:
+                    if i > start:
+                        start = i
+                        visited[text[i]] = i
+                        break
+        return "".join(text[i] for i in sorted(visited.values()))
+
+
+if __name__ == "__main__":
+    # txt = "leetcode"
+    # txt = "cbaacabcaaccaacababa"
+    # txt = "cdadabcc"
+
+    # txt = "ojmgznybdllqahkblcickrmsfrppubzjkjxkcszeqprtnrmtpnqislrcxekuuuzbtsipvtujjulzkxaudxdhegzojdqzsyfuecfrpvqdhyyvxskpicqfyomeogckoagnzowhuogpqgueqgmtddtjepyhiphwkhlvjibnfpelhtffearwjusxnusrlabbgewycildblnepauwwpuhdxcdbbhhohadutuvkhoaafjohrvxeerfxknivvkavznkgvwfacojnkjcuqxzazeudtykpymeyiggujcithaczexffuzuxhkaqtjxbavxsmrgaektyfgtiaqjjkmegmkumiivlihlbhplybhrqmkoeohaflnbpsfaseuupyuumdgfckebamqxfamgaidrbkja"
+    txt = "abcd"
+    print(Solution().smallestSubsequence(txt))
+    print(Solution().smallestSubsequence("xqxxsryqllplyrqoolpsspyqxr"))
